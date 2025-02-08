2812785eefd540079e0a06f6e40d0275d079300c
unknown <jhoncarlosuan26@gmail.com>
2025-02-08 17:22:20 +0800

    updated commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,67 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tailwind in Laravel</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @vite('resources/css/app.css')
 </head>
-<body class="bg-black text-center p-10">
-    <h1 class="text-4xl font-bold text-blue-600">Hello, TailwindCSS in Laravel!</h1>
+<body>
+<div class="overflow-y-scroll bg-white h-screen">
+    <header class="text-gray-600 body-font bg-yellow-500 ">
+  <div class="container mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center">
+    <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+      <img src="/images/eeeclogo.png" class="ml-2 rounded-full w-20 h-20 object-cover "/>
+      <span class="ml-3 text-xl">EEEC HARDWARE</span>
+    </a>
+    <nav class="md:mr-auto md:ml-4 md:py-1  md:pl-4 md:border-l md:border-black	flex flex-wrap items-center text-base justify-center">
+    <a class="mr-5 hover:text-gray-900">First Link</a>
+      <a class="mr-5 hover:text-gray-900">Second Link</a>
+      <a class="mr-5 hover:text-gray-900">Third Link</a>
+      <a class="mr-5 hover:text-gray-900">Fourth Link</a>
+    </nav>
+    <div class="space-x-5 px-4">
+    <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base -mt-4 md:mt-0">Login
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-bar-right ml-2" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M6 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H6.5A.5.5 0 0 0 6 8m-2.5 7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5"/>
+</svg>
+    </button>
+    <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 space-x-10 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Register
+    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box-arrow-in-right ml-2" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0z"/>
+  <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
+</svg>
+    </button>
+    </div>
+  </div>
+</header>
+
+<section class="snap-start">
+            <main style="background-image: url('http://127.0.0.1:8000/images/hardwarebg.jpg');"
+            class="sm:block sm:justify-center bg-eeec bg-cover bg-center sm:items-center">
+           
+          <div class="md:text-center bg-black bg-opacity-80 p-5">
+            
+            <h1 class="animate-blinks duration-700 text-[40px] ml-[6rem] text-left font-bold text-white">SHOP NOW</h1>
+              <h2 class="text-lg animate-blinksssss font-medium  text-left ml-[5.5rem] px-2 rounded-md opacity-80 text-gray-400">
+              Empowering Your Projects with Top-Quality Hardware, <br/>Reliable Service, and Innovative Solutions to Build, <br/>Repair, and Create with Confidence.</h2>
+            
+            
+          </div>
+          
+          
+            
+            
+              
+            
+            
+          
+          </main>
+            </section>
+
+<section class="snap-start ">
+    <main>
+    <h1>Continuing...</h1>
+    </main>
+    </section>
+    </div>
+
 </body>
 </html>
